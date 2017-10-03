d2f32cbfadbfdc041026f86a222cdcaf0da20a26
Michael Morris-Pearce <mikemp109@gmail.com>
2017-10-03 00:33:18 -0700

    Replaced diagram library with raw bitmap.

--- app/Main.hs
+++ app/Main.hs
@@ -6,31 +6,39 @@ module Main where
 
 import Mandelbrot
 
+import Control.Parallel.Strategies
+
 import Data.Complex
 import Data.Maybe
 
-import Diagrams.Prelude
-import Diagrams.Backend.SVG.CmdLine
-import Diagrams.TwoD.Layout.Grid
+import Data.Word
 
-import Data.Colour (withOpacity)
-import Data.Colour.Palette.Harmony
+import Data.Colour.SRGB (toSRGB)
+import Data.Colour.RGBSpace
 import Data.Colour.Palette.BrewerSet
 
-main = mainWith plot
+import Data.ByteString (ByteString, pack, empty, append)
+import Data.ByteString.Conversion
+import Codec.BMP
 
+-- Number of samples per dimension.
+resolution = 400
+
+-- How many samples to calculate in each parallel unit of work.
+chunkSize = 200
+
+-- Interesting coordinates.
+trident = (-0.1011) :+ 0.9563
+armpit = (-0.75) :+ 0
+
+imageLocus = trident
+imageScale = 0.05
+
+palette :: [Kolor]
 palette = reverse set ++ set where
   set = brewerSet style size
   style = BuPu
   size = 9
-
-resolution = 100
-
-trident = (-0.1011) :+ 0.9563
-armpit = (-0.75) :+ 0
-
-imageLocus = 0 :+ 0.75
-imageScale = 0.75
 
 xMin = realPart imageLocus - imageScale
 xMax = realPart imageLocus + imageScale
@@ -39,21 +47,26 @@ yMin = imagPart imageLocus - imageScale
 yMax = imagPart imageLocus + imageScale
 yInc = (yMax - yMin) / resolution
 
-showPeriod = True
+samples = [x :+ y | y <- ys, x <- xs] where
+  xs = tail [xMin, (xMin + xInc) .. xMax]
+  ys = tail [yMin, (yMin + yInc) .. yMax]
 
-plot :: Diagram B
-plot = gridCat' (resolution + 1) $ map drawPoint [(x, y) | y <- reverse ys, x <- xs] where
-  drawPoint (x, y) =
-    let
-      c = (x :+ y)
-      d = divergence c
-    in
-      if d < iterations
-        then square 1 # fc (palette !! (d `mod` length palette)) # lw none
-        else
-          (if showPeriod
-            then text (show $ fromMaybe 0 (period c)) # fc white
-            else mempty)
-          `atop` square 1 # fc black # lw none 
-  xs = [xMin, (xMin + xInc) .. xMax]
-  ys = [yMin, (yMin + yInc) .. yMax]
+divMap :: [Complex Double] -> [Int]
+divMap = withStrategy (parListChunk chunkSize rpar) . map divergence
+
+-- Assign a color to a divergence value in [0..iterations].
+colorify :: Int -> Kolor
+colorify d = palette !! (d `mod` length palette)
+
+normalizeColor :: Double -> Word8
+normalizeColor val = fromIntegral $ floor (val * 255)
+
+packColor :: Kolor -> ByteString
+packColor c = let rgb = toSRGB c
+  in pack $ normalizeColor <$> [channelRed rgb, channelGreen rgb, channelBlue rgb, 0]
+
+byteArray :: ByteString
+byteArray = foldl append empty $ packColor . colorify <$> divMap samples
+
+main = let bitmap = packRGBA32ToBMP resolution resolution byteArray
+  in writeBMP "/dev/stdout" bitmap 
